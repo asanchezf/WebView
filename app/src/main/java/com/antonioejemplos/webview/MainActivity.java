0d2cc6ced7325f4626607318e9cdcef7403cc437
a.sanchez <antoniom.sanchezf@gmail.com>
2015-11-10 20:44:56 +0100

    Se modifica diseño añadiendo un CollpasingToobar y un CardView

--- app/src/main/java/com/antonioejemplos/webview/MainActivity.java
+++ app/src/main/java/com/antonioejemplos/webview/MainActivity.java
@@ -1,6 +1,8 @@
 package com.antonioejemplos.webview;
 
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -10,6 +12,8 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,29 +27,56 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
-         txtBuscar=(EditText)findViewById(R.id.txtBuscar);
+
+        CollapsingToolbarLayout collapsingToolbar =
+                (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbar.setTitle(getString(R.string.app_name));
+
+
+        txtBuscar=(EditText)findViewById(R.id.txtBuscar);
+//        InputMethodManager imm = (InputMethodManager)getSystemService(
+//                Context.INPUT_METHOD_SERVICE);
+//        //check if no view has focus:
+//        View view = this.getCurrentFocus();
+//        if (view == null)
+//            return;
+//        imm.hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+
+
         btnBuscar=(Button)findViewById(R.id.btnBuscar);
         wbBuscar=(WebView)findViewById(R.id.webViewBuscar);
 
-        //Activamos javascript
+
+        //Configuramos el navegador:utf-8, javascript...
+        //String htmlString="html"; //**Your html string**
         WebSettings configura=wbBuscar.getSettings();
         configura.setJavaScriptEnabled(true);
+        configura.setDefaultTextEncodingName("utf-8");
+
+        //String base64 = Base64.encodeToString(htmlString.getBytes(), Base64.DEFAULT);
+        //wbBuscar.loadData(base64, "text/html; charset=utf-8", "base64");
+
+//txtBuscar.getText().toString().toLowerCase(Locale.getDefault());
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                wbBuscar.loadUrl("http://lema.rae.es/drae/srv/search?val=" + txtBuscar.getText().toString().toLowerCase(Locale.getDefault()));
+
+//                Snackbar.make(v, "Navega por la pantalla para ver el resultado de la búsqueda...", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+            }
+        });
 
 
         btnBuscar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                wbBuscar.loadUrl("http://lema.rae.es/drae/srv/search?val=" + txtBuscar.getText().toString());
+                wbBuscar.loadUrl("http://lema.rae.es/drae/srv/search?val=" + txtBuscar.getText().toString().toLowerCase(Locale.getDefault()));
 
+//                Snackbar.make(view, "Navega por la pantalla para ver el resultado de la búsqueda...", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
 
             }
         });
